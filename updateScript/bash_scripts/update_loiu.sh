41383ed42fb6d0ea7930463a1de0ce725156685f
niconara <niconaranjo95@gmail.com>
2024-07-24 17:09:49 -0500

    test by sending the json updated

--- updateScript/bash_scripts/update_loiu.sh
+++ updateScript/bash_scripts/update_loiu.sh
@@ -14,5 +14,14 @@ NEW_VERSION=$(echo $CURRENT_VERSION | sed "s/\.[0-9]*$/.$NEW_Z/")
 # Update the version in the JSON file
 jq --arg new_version "$NEW_VERSION" '.name = $new_version' $VERSION_FILE > temp.json && mv temp.json $VERSION_FILE
 
+# Get last message from git
+LAST_COMMIT_MSG=$(git log -1 --pretty=%B)
+
 # Print the new version
 echo "Version updated to: $NEW_VERSION"
+
+# Stage the version change
+git add $VERSION_FILE
+
+# Amend the previous commit with the version update
+git commit --amend -m "$LAST_COMMIT_MSG"
