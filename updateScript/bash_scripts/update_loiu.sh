ec1bd7145f9651b2fc932a8d3cec68e1d3b322da
niconara <niconaranjo95@gmail.com>
2024-07-24 17:47:24 -0500

    Fetching from master and getting the update script running

--- updateScript/bash_scripts/update_loiu.sh
+++ updateScript/bash_scripts/update_loiu.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
-# Loui JSON file
+
+# Get first last update from the document
+git fetch --depth=1 origin master
+git checkout origin/master -- ./updateScript/loui.json
+
+
+# Nombre del archivo JSON
 VERSION_FILE="./updateScript/loui.json"
 
-# Reed the actual version of the file
+# Leer la versión actual desde el archivo JSON
 CURRENT_VERSION=$(jq -r '.name' $VERSION_FILE)
+
+# Loui JSON file
 
 # Increment the version
 # The version is in the format X.Y.Z and increments Z
-OLD_Z=$(echo $CURRENT_VERSION | cut -d '.' -f 3)
+OLD_VERSION=$CURRENT_VERSION
+OLD_Z=$(echo $OLD_VERSION | cut -d '.' -f 3)
 NEW_Z=$((OLD_Z + 1))
-NEW_VERSION=$(echo $CURRENT_VERSION | sed "s/\.[0-9]*$/.$NEW_Z/")
+NEW_VERSION=$(echo $OLD_VERSION | sed "s/\.[0-9]*$/.$NEW_Z/")
 
 # Update the version in the JSON file
 jq --arg new_version "$NEW_VERSION" '.name = $new_version' $VERSION_FILE > temp.json && mv temp.json $VERSION_FILE
